7224fb7d9fc3882fee4dba1ab29287984153bed4
Cyan903 <cyanp0515@gmail.com>
2022-08-27 22:42:25 -0400

    [+] chore: Rework storage

--- pages/index.js
+++ pages/index.js
@@ -7,28 +7,40 @@ const endpoints = {
 };
 
 const conf = {
-    "anilist-score": false,
-    "blur-cover": false,
-    "show-upcoming": false,
+    settings: {
+        // info
+        "anilist-score": false,
+        "blur-cover": false,
+        "show-upcoming": false,
 
-    "episode-bookmark": false,
-    "season-number": false,
-    "highest-resolution": false,
+        // watch
+        "episode-bookmark": false,
+        "season-number": false,
+        "highest-resolution": false,
 
-    "random-anime": false,
-    "fakesite-ignore": false,
+        // general
+        "random-anime": false,
+        "fakesite-ignore": false,
+    },
+
+    // saved episodes
+    saved: [],
 };
 
 (async () => {
     // load storage
-    for (const o of Object.keys(conf)) {
-        conf[o] = !!(await browser.storage.sync.get(o))[o];
+    const settings = await browser.storage.sync.get("settings");
+
+    for (const o of Object.keys(conf.settings)) {
+        if (Object.keys(settings).length != 0) {
+            conf.settings[o] = settings.settings[o];
+        }
     }
 
     // execute functions
     for (const func of Object.keys(pahe)) {
         try {
-            if (conf[func]) await pahe[func]();
+            if (conf.settings[func]) await pahe[func]();
         } catch (e) {
             console.warn(`[pahe-plus] internal error ${e}`);
         }
